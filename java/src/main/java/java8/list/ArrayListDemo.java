77e0ac7902ee4d5f87c5b15b8cf52eca3526a92d
Saravnan M <vmsaravanan27@gmail.com>
2018-04-14 13:56:59 +0530

    Java 8 - Features Learning Project committed.

---
+++ java/src/main/java/java8/list/ArrayListDemo.java
@@ -0,0 +1,16 @@
+package java8.list;
+
+/**
+ * @author Saravanan M
+ * @created on Apr 14, 2018 1:32:55 PM
+ * @description Java8 - ArrayList Demo
+ */
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+
+		System.out.println("Array List Demo");
+		
+		
+	}
+}
